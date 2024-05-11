ed034ef913d3a8fe84aacbe537a16bf049e59c7b
Colossus <triplesonicslash@list.ru>
2024-05-11 12:52:19 +0300

    Initial commit

---
+++ src/main/kotlin/com/colossus/shopmanager/ShopManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.colossus.shopmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShopManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/colossus/shopmanager/ShopManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.colossus.shopmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
